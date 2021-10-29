065e4578ee5cad87b3ad473dee1ff19fe0ed41ef
tsar-boomba <itg.2048@gmail.com>
2021-10-29 13:02:05 -0400

    added example app

---
+++ example/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from 'icicles';
+
+function App() {
+	return (
+		<div className='App'>
+			<HelloWorld />
+		</div>
+	);
+}
+
+export default App;
